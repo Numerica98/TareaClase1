8a6fa6517b4aaa28ad7201a783fd812053ab5aa8
Melissa <Melissa@DESKTOP-5II7QNA>
2018-03-20 21:18:03 -0300

    Tarea1 de clase version final

---
+++ src/datos/Datos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author Melissa
+ */
+public class Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        System.out.println("DATOS GENERALES");
+        System.out.println("Nombres: Nuria Melissa");
+        System.out.println("Apellidos: Rivas Canjura");
+        System.out.println("Edad: 19 años");
+        System.out.println("Género: Femenino");
+        System.out.println("Tipo de sangre: A Positivo");
+        System.out.println("Fecha de nacimiento: 18/06/1998");
+        System.out.println("Estado civil: Soltera");
+        System.out.println("Carnet de estudiante: 00041517");
+        // TODO code application logic here
+    }
+    
+}
